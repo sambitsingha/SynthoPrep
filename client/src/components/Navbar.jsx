b67d27dfe991afd4fee91c7a5ddc33dd59d29f22
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-04-17 21:15:05 +0530

    Update Navbar component with responsive design and menu functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowTrendDown, faBars } from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
-		<div className='flex items-center my-8 mx-10'>
-			<img src={logo} alt='' className='w-1/4' />
-			<div className='w-full'>
-				<ul className='flex justify-center items-center gap-40'>
-					<li>About Us</li>
-					<li>Dashboard</li>
-					<li>Contact</li>
-				</ul>
+		<div className='flex items-center justify-around m-3 gap-32'>
+			<img src={logo} alt='' className='md:w-80 w-40' />
+			<div className='relative sm:hidden md:hidden block'>
+				<button onClick={() => setIsOpen(!isOpen)}>
+					<FontAwesomeIcon icon={faBars} />
+				</button>
+				<div
+					className={`absolute right-0 w-40 mt-2 py-2 bg-white border rounded shadow-xl ${
+						isOpen ? "block" : "hidden"
+					}`}>
+					<a
+						href=''
+						className='transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white'>
+						About Us
+					</a>
+					<a
+						href=''
+						className='transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white'>
+						Dashboard
+					</a>
+					<a
+						href=''
+						className='transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white'>
+						Contact
+					</a>
+				</div>
 			</div>
-			<button>Try out</button>
+			<div className='hidden md:flex justify-center items-center gap-20 text-xl'>
+				<a href='' className='text-normal text-gray-900 hover:text-purple-500'>
+					About Us
+				</a>
+				<a href='' className='text-normal text-gray-900 hover:text-purple-500'>
+					Dashboard
+				</a>
+				<a href='' className='text-normal text-gray-900 hover:text-purple-500'>
+					Contact
+				</a>
+			</div>
+			<div className='hidden md:flex justify-center items-center'>
+				<button className='w-20 font-bold text-xl'>Try out</button>
+				<FontAwesomeIcon icon={faArrowTrendDown} />
+			</div>
 		</div>
 	);
 };
