4c41f98b3a87427e1489bfd8ce50ff28778dcc97
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-04-18 23:47:16 +0530

    Update Hero component styling in Homepage

--- client/src/components/Homepage/Hero.jsx
+++ client/src/components/Homepage/Hero.jsx
@@ -8,21 +8,23 @@ const Hero = () => {
 			<Navbar />
 			<div className='mx-20'>
 				<div className='hero-text flex flex-col'>
-					<span className='-mb-16 pl-16 text-8xl uppercase text-left'>
+					<span className='line-1 -mb-16 pl-16 text-8xl uppercase text-left'>
 						Learn
 					</span>
-					<span className='-mb-16 uppercase text-[16rem] text-center'>
+					<span className='line-2 -mb-16 uppercase text-[16rem] text-center'>
 						The <span className='text-gradient'>Future</span>
 					</span>
-					<span className=' pr-16 uppercase text-8xl text-right'>way</span>
+					<span className='line-3 pr-16 uppercase text-8xl text-right'>
+						way
+					</span>
 				</div>
-				<p className='mx-20 w-1/3 -mt-20'>
+				<p className='line-4 mx-20 w-1/3 -mt-20'>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum iusto
 					odit vel molestias magni, unde ipsa laboriosam quos fugiat enim
 					voluptas amet nihil, voluptatem repellat tempora minima sint
 					architecto! Ducimus.
 				</p>
-				<button className='gradient mx-16 uppercase bg-sky-500 my-5 py-4 px-10 rounded-xl text-white'>
+				<button className='button gradient mx-16 uppercase bg-sky-500 my-5 py-4 px-10 rounded-xl text-white'>
 					Try For Free <FontAwesomeIcon icon={faArrowTrendDown} />
 				</button>
 			</div>
