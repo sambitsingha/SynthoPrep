bddd75b272cebe45d9d71a8e91a184c33dd6c38f
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-04-18 23:47:10 +0530

    Update About component in Homepage

--- client/src/components/Homepage/About.jsx
+++ client/src/components/Homepage/About.jsx
@@ -9,11 +9,18 @@ const About = () => {
 				<FontAwesomeIcon icon={faArrowTrendDown} className='text-sky-600' />
 			</button>
 			<div className='mx-20 px-10'>
-				<h1 className=' text-8xl hero-text uppercase'>
-					<span>Sharpen your edge with top</span>
-					<span>-tier questions, turbocharge </span>
-					<span>your prep with Our</span> Advanced AI model.
+				<h1 className=' text-8xl hero-text uppercase '>
+					<span className='line-1'>Sharpen your edge with top</span>
+					<span className='line-2'>-tier questions, turbocharge </span>
+					<span className='line-3'>your prep with Our</span>{" "}
+					<span className='line-4'>Advanced AI model.</span>
 				</h1>
+				<p className='float-right w-1/3'>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel
+					assumenda voluptate fugiat eveniet dicta, aliquid beatae tenetur
+					aperiam impedit porro tempora earum expedita repellat doloribus
+					dolorum et eaque distinctio ex!
+				</p>
 			</div>
 		</div>
 	);
