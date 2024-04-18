8644f661518596bba2c86321e7366de170c8539c
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-04-18 21:08:40 +0530

    Update Hero component styling in Homepage

--- client/src/components/Homepage/Hero.jsx
+++ client/src/components/Homepage/Hero.jsx
@@ -4,19 +4,19 @@ import { faArrowTrendDown } from "@fortawesome/free-solid-svg-icons";
 import Navbar from "../Navbar";
 const Hero = () => {
 	return (
-		<div className='m-8 p-2 rounded-3xl hero'>
+		<div className='bg'>
 			<Navbar />
 			<div className='mx-20'>
-				<div className='hero flex flex-col'>
-					<span className='-mb-16 pl-3 text-8xl uppercase text-left'>
+				<div className='hero-text flex flex-col'>
+					<span className='-mb-16 pl-16 text-8xl uppercase text-left'>
 						Learn
 					</span>
 					<span className='-mb-16 uppercase text-[16rem] text-center'>
 						The <span className='text-gradient'>Future</span>
 					</span>
-					<span className=' pr-4 uppercase text-8xl text-right'>way</span>
+					<span className=' pr-16 uppercase text-8xl text-right'>way</span>
 				</div>
-				<p className='mx-12 w-1/3 -mt-20'>
+				<p className='mx-20 w-1/3 -mt-20'>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum iusto
 					odit vel molestias magni, unde ipsa laboriosam quos fugiat enim
 					voluptas amet nihil, voluptatem repellat tempora minima sint
