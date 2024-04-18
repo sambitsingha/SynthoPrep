4a52a51ceef11f0bc6bef658c301645213e7f1a7
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-04-18 23:46:53 +0530

    Add ScrollRevealComponent to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
+import ScrollRevealComponent from "./ScrollReveal";
 import Home from "./components/Homepage/Home";
 const App = () => {
 	return (
 		<>
 			<Home />
+			<ScrollRevealComponent />
 		</>
 	);
 };
