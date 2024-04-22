6614f5dedaeb9b642b459a0a1ae3148ef69663b5
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-04-22 09:46:58 +0530

    Catalog componet added

--- client/src/components/Homepage/Home.jsx
+++ client/src/components/Homepage/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./Hero";
 import About from "./About";
+import Catalog from "./Catalog";
 const Home = () => {
 	return (
 		<>
@@ -9,6 +10,9 @@ const Home = () => {
 			</section>
 			<section className='h-screen snap-center'>
 				<About />
+			</section>
+			<section className='h-screen snap-center'>
+				<Catalog />
 			</section>
 		</>
 	);
