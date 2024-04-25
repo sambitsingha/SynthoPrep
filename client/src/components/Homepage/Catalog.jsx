0fb2cacf27997a99cfcd61a8afbee58b0d825eba
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-04-26 00:04:22 +0530

    Refactor Catalog component styling and content in Homepage

--- client/src/components/Homepage/Catalog.jsx
+++ client/src/components/Homepage/Catalog.jsx
@@ -8,18 +8,18 @@ const Catalog = () => {
 				Catalog{" "}
 				<FontAwesomeIcon icon={faArrowTrendDown} className='text-sky-600' />
 			</button>
-			<h1 className='uppercase hero-text text-7xl mx-20'>
+			<h1 className='line-1 uppercase hero-text text-7xl mx-20'>
 				Choose your <span className='text-gradient'>path</span>
 			</h1>
 			<hr className='h-[2px] w-11/12 m-auto my-16 bg-gray-900' />
 			<div className='flex flex-col'>
 				<div className='flex justify-center items-start'>
-					<h1 className='text-7xl hero-text uppercase'>free</h1>
+					<h1 className='line-2 text-7xl hero-text uppercase'>free</h1>
 					<div className='flex flex-col justify-center items-center'>
-						<h1 className='text-4xl hero-text mb-5 uppercase text-center'>
+						<h1 className='line-2 text-4xl hero-text mb-5 uppercase text-center'>
 							Perks
 						</h1>
-						<ul className='text-2xl w-2/3 text-center'>
+						<ul className='line-3 text-2xl w-2/3 text-center'>
 							<li className='mb-5'>Get access to our AI model</li>
 							<li>
 								Get access to the Community where you can connect with fellow
@@ -28,8 +28,10 @@ const Catalog = () => {
 						</ul>
 					</div>
 					<div>
-						<p className='text-4xl hero-text uppercase'>Duration</p>
-						<h1 className='text-2xl hero-text text-center mt-5'>Life Time</h1>
+						<p className='line-2 text-4xl hero-text uppercase'>Duration</p>
+						<h1 className='line-3 text-2xl hero-text text-center mt-5'>
+							Life Time
+						</h1>
 					</div>
 				</div>
 			</div>
