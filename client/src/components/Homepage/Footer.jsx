3f69bffbf3301b4d4cdabee9d364d46a11eea7d8
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-04-26 21:31:47 +0530

    Update Footer component styling and content in Homepage

--- client/src/components/Homepage/Footer.jsx
+++ client/src/components/Homepage/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Footer = () => {
 	return (
 		<>
-			<h1>Footer</h1>
+			<div className='bg-gray-200 '>
+				<h1 className='h-12 flex justify-center items-center m-auto font-semibold text-lg'>
+					SynthoPrep Platform: SynthoPrep - Crafting solutions
+				</h1>
+			</div>
 		</>
 	);
 };
