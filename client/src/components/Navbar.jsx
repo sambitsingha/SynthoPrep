2a4bfd870da7cbc3136452ea51febc09751559d2
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-04-26 23:04:34 +0530

    Update Navbar component link in Homepage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 						isOpen ? "block" : "hidden"
 					}`}>
 					<a
-						href=''
+						href='#about'
 						className='transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white'>
 						About Us
 					</a>
