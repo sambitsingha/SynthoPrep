41f9981d6f250104fdfdd0e0d31660416736ed01
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-04-28 17:06:05 +0530

    Update Navbar component link and add new logo in Homepage

--- client/src/components/Homepage/Home.jsx
+++ client/src/components/Homepage/Home.jsx
@@ -3,21 +3,23 @@ import Hero from "./Hero";
 import About from "./About";
 import Catalog from "./Catalog";
 import Footer from "./Footer";
+import Navbar from "../Navbar";
 const Home = () => {
 	return (
 		<>
-			<section className='h-screen snap-center'>
+			<section className='h-auto md:h-screen snap-center bg'>
+				<Navbar />
 				<Hero />
 			</section>
-			<section className='h-screen snap-center' id='about'>
+			<section className='h-auto md:h-screen snap-center'>
 				<About />
 			</section>
-			<section className='h-screen snap-center'>
+			{/* <section className='h-screen snap-center'>
 				<Catalog />
 			</section>
 			<section className='snap-center'>
 				<Footer />
-			</section>
+			</section> */}
 		</>
 	);
 };
