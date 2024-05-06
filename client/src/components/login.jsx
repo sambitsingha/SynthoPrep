2b7d05eb5cc5dd4f85317339c7e735e575a43348
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-05-06 21:05:54 +0530

    responsiveness added

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -3,15 +3,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle, faMicrosoft } from "@fortawesome/free-brands-svg-icons";
 const Login = () => {
 	return (
-		<div className='w-full flex justify-center items-center'>
-			<div className='w-1/2'>
+		<div className='w-full flex flex-col-reverse lg:flex-row justify-center items-center'>
+			<div className='w-full lg:w-1/2'>
 				<img
 					src='https://rtrfm.com.au/wp-content/uploads/2021/04/Essential-Books.jpg'
 					alt=''
-					className='w-full h-screen'
+					className='w-full lg:h-screen'
 				/>
 			</div>
-			<div className='bg-black h-screen w-1/2 flex justify-center items-center font-semibold text-white flex-col gap-4 px-28'>
+			<div className='bg-black h-screen w-full lg:w-1/2 flex justify-center items-center font-semibold text-white flex-col gap-4 px-10 md:px-24 lg:px-28'>
 				<h1 className='text-4xl text-center '>Log in</h1>
 				<p>Sign in to track your progress</p>
 				<form
@@ -26,7 +26,7 @@ const Login = () => {
 					<label>Enter your student password</label>
 					<input
 						type='email'
-						placeholder='...................'
+						placeholder='password'
 						className='w-full flex items-center justify-center rounded-3xl px-5 h-10 bg-gray-800/80 border-none outline-none'
 					/>
 				</form>
