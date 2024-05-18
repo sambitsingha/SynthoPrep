03f794168c578881dbdeccd09f2685c2d3b09546
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-05-18 10:51:27 +0530

    Custom 404 routing added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import ScrollRevealComponent from "./ScrollReveal";
 import Home from "./components/Homepage/Home";
 import Login from "./components/login";
 import DashboardLayout from "./components/dashboard/layout";
+import Custom404 from "./components/Custom404";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 const App = () => {
 	return (
 		<>
@@ -11,10 +13,12 @@ const App = () => {
 					<Route path='/' element={<Home />} />
 					<Route path='/login' element={<Login />} />
 					<Route path='/dashboard' element={<DashboardLayout />} />
+					<Route path='*' element={<Custom404 />} />
 				</Routes>
 			</Router>
 			<ScrollRevealComponent />
 		</>
 	);
 };
+
 export default App;
