8d3bb97748eb8e6892063f7c475b45c1a05ba8a3
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-05-22 21:53:44 +0530

    Updated login and created signup page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle, faMicrosoft } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
-const Login = () => {
+const Signup = () => {
 	return (
 		<div className='w-full flex flex-col-reverse lg:flex-row justify-center items-center'>
 			<div className='w-full lg:w-1/2'>
@@ -12,8 +12,8 @@ const Login = () => {
 				/>
 			</div>
 			<div className='bg-black h-screen w-full lg:w-1/2 flex justify-center items-center font-semibold text-white flex-col gap-4 px-10 md:px-24 lg:px-28'>
-				<h1 className='text-4xl text-center '>Log in</h1>
-				<p>Sign in to track your progress</p>
+				<h1 className='text-4xl text-center '>Sign Up</h1>
+				<p>Create an account here</p>
 				<form
 					action=''
 					className='flex flex-col w-full gap-2 mt-5 justify-center '>
@@ -23,18 +23,22 @@ const Login = () => {
 						placeholder='email@gmail.com'
 						className='w-full flex items-center justify-center rounded-3xl px-5 h-10 bg-gray-800/80 border-none outline-none'
 					/>
-					<label>Enter your student password</label>
+					<label>Set your password</label>
+					<input
+						type='email'
+						placeholder='password'
+						className='w-full flex items-center justify-center rounded-3xl px-5 h-10 bg-gray-800/80 border-none outline-none'
+					/>
+					<label>Confirm your password</label>
 					<input
 						type='email'
 						placeholder='password'
 						className='w-full flex items-center justify-center rounded-3xl px-5 h-10 bg-gray-800/80 border-none outline-none'
 					/>
 				</form>
-				<a href=''>
-					<h1>Forget your password ?</h1>
-				</a>
+
 				<button className='bg-red-500 w-full h-10 rounded-3xl border-none outline-none duration-300 hover:bg-red-900'>
-					Sign In
+					Sign Up
 				</button>
 				<div className='flex justify-center items-center gap-2'>
 					<hr className='bg-white h-[1px] w-40' />
@@ -50,11 +54,11 @@ const Login = () => {
 					</button>
 				</div>
 				<p>
-					Create a new account.{" "}
+					Already have an account.{" "}
 					<Link
-						to='/signup'
+						to='/login'
 						className='hover:text-xl duration-200 ease-linear hover:text-green-600'>
-						Sign up here
+						Login here
 					</Link>
 				</p>
 			</div>
@@ -62,4 +66,4 @@ const Login = () => {
 	);
 };
 
-export default Login;
+export default Signup;
